465e5e7ea41455690870f529b25f693323ad1f29
Ryan Mathews <ryan@ryanjm.com>
2015-08-07 21:34:21 -0600

    Added nombom

--- oh-my-zsh/custom/plugins/ryanjm/ryanjm.plugin.zsh
+++ oh-my-zsh/custom/plugins/ryanjm/ryanjm.plugin.zsh
@@ -2,7 +2,7 @@ echo "Loading ryanjm plugin"
 
 alias cls='clear; ls'
 alias clsa='clear; lsa'
-
+alias nombom='npm cache clear && bower cache clean && rm -rf node_modules bower_components && npm install && bower install'
 # https://gist.github.com/piscisaureus/3342247
 function pullify() {
   git config --add remote.origin.fetch '+refs/pull/*/head:refs/remotes/origin/pr/*'
